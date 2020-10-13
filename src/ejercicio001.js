e34e8237cfd7f01e25ea9755a3fd13f8a5419263
mvega <mvega@iespuntadelverde.es>
2020-10-13 20:27:59 +0200

    first commit

--- src/ejercicio001.js
+++ src/ejercicio001.js
@@ -3,4 +3,4 @@ db.amigos.find({"edad":   {$eq: 20 } }).pretty()
 // $gt para campos con un valor mayor al especificado
 db.amigos.find({"km de distancia": {$gt: 30}}).pretty()
 // $lt seleccionará todos los documentos de la colección de inventario donde el valor del campo de cantidad es menor que...
-db.amigos.find( { "redes sociales": { $lt: 3 } } )
+db.amigos.find({"km de distancia": {$lt: 26}, "redes sociales": { $lt: 3 } }).pretty()
